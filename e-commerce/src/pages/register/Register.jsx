c577716f1c53f97a57f4463655fc19db892e6f5d
adwaithek <adwaithek01@gmail.com>
2023-03-09 00:14:16 +0530

    first commit

--- e-commerce/src/pages/register/Register.jsx
+++ e-commerce/src/pages/register/Register.jsx
@@ -4,75 +4,96 @@
 
 
 
+// import React, { useState } from "react";
+// import axios from "axios";
+// import { useNavigate } from "react-router-dom";
+// import toast from "react-hot-toast";
+// import Layout from "../../components/Layout/layout/Layout";
 
+// function Register(){
+//   const [name, setName] = useState("");
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+  
+//   const navigate = useNavigate();
 
-// import React, { useState, useContext } from 'react'
-// import {toast} from 'react-toastify';
-// import axios from 'axios'
-// import {useNavigate} from 'react-router-dom' 
-
-//  import "./register.css"
-
-//  function Register() {
-
-//     const [name,setName]=useState("")
-//     const [email,setEmail]=useState("")
-//     const [password,setPassword]=useState("")
-//     const navigate=useNavigate()
-
-
-   
-//    const handleSubmit =async (e) => {
+//   // form function
+//   const handleSubmit = async (e) => {
 //     e.preventDefault();
-
-//     try{
-//         const res=await axios.post('/api/v1/auth/register',{
-//             name,email,password
-//         });
-//         if(res.data.success){
-// toast.success(res.data.message)
-// navigate('/')
-//         }else{
-//           alert(res.data.error)
-//         }
-//     }catch(error){
-//       console.log(error);
-//       toast.error("something went wrong")
-
-//     }
-    
-//     // toast.success('Register Successfully')
-//    }
-    
-
-//  return(
-//    <div className="bg">
-//    <div className="wrapper">
-//        <form className="login" onSubmit={handleSubmit}>
-//          <p className="title">REGISTER</p>
-//          <input  type='email' placeholder='email' required id='email' onChange={(e)=>setEmail(e.target.value)}  />
-//          {/* <i></i> */}
-//          <input onChange={(e)=>setName(e.target.value)} id="username" className="username" type="text" placeholder="Username" autofocus required />
-//          {/* <i className="fa fa-user" /> */}
-//          <input  onChange={(e)=>setPassword(e.target.value)}  id="password" className="password"  type="password" placeholder="Password" required />
-//          {/* <i className="fa fa-key" /> */}
-          
-
-//          <a >Forgot your password?</a>
-//          <button>
-//            <i className="spinner" />
-//            <span   className="">CREATE</span>
-//          </button>
-//        </form>
+//     try {
+//       const res = await axios.post("/api/v1/auth/register", {
+//         name,
+//         email,
+//         password,
        
-//        <p />
-//      </div>
-//      </div>
-//    );
-//  }
+//       });
+//       if (res && res.data.success) {
+//         toast.success(res.data && res.data.message);
+//         navigate("/login");
+//       } else {
+//         toast.error(res.data.message);
+//       }
+//     } catch (error) {
+//       console.log(error);
+//       toast.error("Something went wrong");
+//     }
+//   };
+
+//   return (
+    
+//     <div>
+//       <div className="form-container" style={{ minHeight: "90vh" }}>
+//         <form onSubmit={handleSubmit}>
+//           <h4 className="title">REGISTER FORM</h4>
+//           <div className="mb-3">
+//             <input
+//               type="text"
+//               value={name}
+//               onChange={(e) => setName(e.target.value)}
+//               className="form-control"
+//               id="exampleInputEmail1"
+//               placeholder="Enter Your Name"
+//               required
+//               autoFocus
+//             />
+//           </div>
+//           <div className="mb-3">
+//             <input
+//               type="email"
+//               value={email}
+//               onChange={(e) => setEmail(e.target.value)}
+//               className="form-control"
+//               id="exampleInputEmail1"
+//               placeholder="Enter Your Email "
+//               required
+//             />
+//           </div>
+//           <div className="mb-3">
+//             <input
+//               type="password"
+//               value={password}
+//               onChange={(e) => setPassword(e.target.value)}
+//               className="form-control"
+//               id="exampleInputPassword1"
+//               placeholder="Enter Your Password"
+//               required
+//             />
+//           </div>
+         
+//           <button type="submit" className="btn btn-primary">
+//             REGISTER
+//           </button>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Register;
 
 
-//  export default Register;
+
+
 
 
 
@@ -81,9 +102,11 @@
 
 import React, { useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 import Layout from "../../components/Layout/layout/Layout";
+import './register.css'
+
 
 function Register(){
   const [name, setName] = useState("");
@@ -115,54 +138,39 @@ function Register(){
   };
 
   return (
-    
-    <div>
-      <div className="form-container" style={{ minHeight: "90vh" }}>
-        <form onSubmit={handleSubmit}>
-          <h4 className="title">REGISTER FORM</h4>
-          <div className="mb-3">
-            <input
+    <div className="bg">
+   <div className="wrapper">
+       <form className="register" onSubmit={handleSubmit}>
+         <p className="title">Register</p>
+         <input
               type="text"
               value={name}
               onChange={(e) => setName(e.target.value)}
               className="form-control"
               id="exampleInputEmail1"
-              placeholder="Enter Your Name"
+              placeholder="Enter your name"
               required
               autoFocus
             />
-          </div>
-          <div className="mb-3">
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="form-control"
-              id="exampleInputEmail1"
-              placeholder="Enter Your Email "
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="form-control"
-              id="exampleInputPassword1"
-              placeholder="Enter Your Password"
-              required
-            />
-          </div>
+         <input  type="email" autoFocus value={email} onChange={(e)=>setEmail(e.target.value)} id="email" className="username"  placeholder="Enter your email"  required />
+       
+         <input type="password" value={password}  onChange={(e)=>setPassword(e.target.value)} id="password" className="password"  placeholder="enter your password" required />
          
-          <button type="submit" className="btn btn-primary">
-            REGISTER
-          </button>
-        </form>
-      </div>
-    </div>
-  );
-};
+        
+         <a >Alredy have a account ?</a><span><Link to='/login'>Login here</Link></span>
+         <button>
+           <i className="spinner" />
+           <span    className="">Log in</span>
+         </button>
+       </form>
+       <p />
+     </div>
+     </div>
+   );
+ }
+
 
 export default Register;
+
+
 
--- server/server.js
+++ server/server.js
@@ -12,16 +12,25 @@ import productRoutes from './routes/productRoute.js'
 import cors from 'cors'
 
 
+
+import path from 'path'
+
+
+
 dotenv.config()
 
 connectDB();
 
 const app=express()
 
+const __dirname = path.dirname('../e-commerce/build');
+
 //middlewares
 app.use(cors());
 app.use(express.json())
 app.use(morgan('dev'))
+
+app.use(express.static(path.join(__dirname, '../e-commerce/build')))
 
 
 //routes
@@ -30,8 +39,8 @@ app.use('/api/v1/category',CategoryRoutes)
 app.use('/api/v1/product',productRoutes)
  
  
-app.get('/',(req,res)=>{
-    res.send({message:'hi'})
+app.use('*',function(req,res){
+    res.sendFile(path.join(__dirname, '../e-commerce/build/index.html'));
 })
 
 
