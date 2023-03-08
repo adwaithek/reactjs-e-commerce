19c92b50fc6ffc3081a7b53c29261eb1723df46e
adwaithek <113845706+adwaithek@users.noreply.github.com>
2023-03-09 01:44:52 +0530

    update
    ee

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use('/api/v1/category',CategoryRoutes)
 app.use('/api/v1/product',productRoutes)
  
  
-app.use('*',function(req,res){
+app.use("*",function(req,res){
     res.sendFile(path.join(__dirname, '../e-commerce/build/index.html'));
 })
 
